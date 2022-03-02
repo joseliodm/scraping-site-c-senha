087893ef541f79a44d57105679c926bcd9be3c95
joseliodm <80791244+joseliodm@users.noreply.github.com>
2022-03-02 17:56:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,35 +4,60 @@ async function start() {
      async function loadMore(page, selector){
           const moreButton = await page.$(selector)
           if(moreButton) {
-              more = console.log('More');
-              await moreButton.click();
-              await loadMore(page, selector)
-      }
-      
+             more.console.log('clicking more button')
+             await moreButton.click();
+             await loadMore(page, selector);
           }
+      
+}
 
 async function getComments(page, selector){ 
     const comments = await page.$$eval(selector, links => links.map(link => link.innerText))
     return comments
 }
 
-const browser = await puppeter.launch({headless: false});
+const browser = await puppeter.launch();
 const page = await browser.newPage();
 // Acessa o site
-await page.setDefaultNavigationTimeout(0); //Acessa a página sem esperar o timeout
+page.setDefaultNavigationTimeout(0); //Acessa a página sem esperar o timeout
 await page.goto('https://parceirohypera.com.br');
+
 await page.waitForSelector('input[name="usr"]');
+
 await page.type('input[name="usr"]', '',{delay:100}); // Email
+
 await page.type('input[name="psw"]', '', {delay:100}); // Senha
- await page.keyboard.press('Enter');  // Clica no botão de login
+
+await page.keyboard.press('Enter');  // Clica no botão de login
+
  await page.waitForNavigation();
  await page.goto('https://parceirohypera.com.br/ItensProdutoPedido');
+await page.click('#prosseguirPolitCookie');
+
 const comments = await getComments(page,'#DataTables_Table_0 > tbody > tr:nth-child(1) > td.colProduto.pb-4 > div.row.mb-2 > div.col-sm > div:nth-child(2) > a');
 const comments2 = await getComments(page,'#DataTables_Table_0 ');
- await loadMore(page, '#DataTables_Table_0_next');
+
+
+switch(loadMore)
+{
+    case page, selector:
+        
+    await loadMore(page, selector, ('#DataTables_Table_0-next'));
+
+        break;
+    default:
+        
+    console.log('Não há mais');
+    
+    break
+}
+
 console.log(comments);
 console.log(comments2);
 await browser.close();
+
 }
+
+
 
 start();
