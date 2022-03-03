8d831623ab6345a241b0228dd0c52d4c8d15eabc
joseliodm <80791244+joseliodm@users.noreply.github.com>
2022-03-03 15:56:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function getComments(page, selector){
     return comments
 }
 
-const browser = await puppeter.launch( {headless: false} );
+const browser = await puppeter.launch({headless: false});
 const page = await browser.newPage();
 // Acessa o site
 page.setDefaultNavigationTimeout(0); //Acessa a página sem esperar o timeout
@@ -27,208 +27,19 @@ await page.keyboard.press('Enter');  // Clica no botão de login
  
 await page.click('#prosseguirPolitCookie');
 
-  
+let clicou = await page.click('#DataTables_Table_0_next > a');
 
-const comments = await getComments(page,'#DataTables_Table_0');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments2 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments3 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments4 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
+for (clicou = 0; clicou < 59; clicou++) {
+    const comments = await getComments(page, '#DataTables_Table_0 > tbody')
+    await page.click('#DataTables_Table_0_next > a')
+    await page.waitForTimeout(500);
+    const counted = count(comments);
+    sort(counted);
+    console.log(comments);
+    
+}  // Faz o loop até o final da página
 
 
-const comments5 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments6 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments7 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments8 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments9 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments10 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments11 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments12 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments13 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments14 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments15 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments16 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments17 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments18 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments19 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments20 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments21 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments22 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments23 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments24 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments25 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments26 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments27 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments28 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments29 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments30 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments31 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments32 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments33 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments34 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments35 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments36 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments37 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments38 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments39 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments40 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments41 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments42 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments43 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments44 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments45 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments46 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments47 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments48 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments49 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments50 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments51 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments52 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments53 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments54 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments55 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-
-const comments56 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments57 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments58 = await getComments(page,'#DataTables_Table_0 ');
-await page.click('#DataTables_Table_0_next > a');
-await page.waitForTimeout(500);
-const comments59 = await getComments(page,'#DataTables_Table_0 ');
-
-console.log(
-    comments, comments2,comments3, comments4,comments5, comments6,comments7, comments8,comments9, 
-    comments10,comments11, comments12,comments13, comments14,comments15, comments15,comments16,
-    comments17,comments18, comments20,comments21, comments22,comments23, comments24,comments25,
-    comments26,comments27, comments28,comments29,comments29, comments30,comments31, comments32,
-    comments33, comments34,comments35, comments36,comments37, comments38,comments39,comments40,
-    comments41, comments42,comments43, comments44,comments45, comments46,comments47, comments48,
-    comments49, comments50,comments51,comments52,comments53, comments54,comments55,comments56,
-    comments57, comments58,comments59);
 
 await browser.close();
 }
